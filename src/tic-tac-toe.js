9ed4d147ecb8d31a922d466a299c6050289346d1
darkspeaker <graggg13@gmail.com>
2021-09-04 17:00:37 +0300

    feat: complite getFieldValue, nomoreTurns, getCurrent playerS

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,21 @@
 class TicTacToe {
-    constructor() {
+    currentUser = 'x'
+    gameField = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+    ]
 
-    }
+    winner = false
 
     getCurrentPlayerSymbol() {
-
+        return this.currentUser
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if(this.gameField[rowIndex][columnIndex]) return
+        this.gameField[rowIndex][columnIndex] = this.currentUser
+        return this.currentUser = this.currentUser == 'x' ? 'o' : 'x'
     }
 
     isFinished() {
@@ -16,11 +23,11 @@ class TicTacToe {
     }
 
     getWinner() {
-
     }
 
     noMoreTurns() {
-
+        const resExit = this.gameField.map(item => item.find(el => el === null))
+        return resExit.find(item => item === null) === null ? false : true
     }
 
     isDraw() {
@@ -28,7 +35,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.gameField[rowIndex][colIndex]
     }
 }
 
